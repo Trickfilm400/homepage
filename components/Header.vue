99f83758b64c1e6a918d3c870f961e97a79be2a0
Nico Wagner <info@ni-wa.de>
2021-08-27 17:13:42 +0200

    updated header links css

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="header pt-5 sm:pt-2 flex justify-between content-between text-white">
-    <a class="pl-10 sm:pl-3 text-4xl hover:text-gray-300 hover:underline" href='/'>Trickfilm400</a>
+    <a class="pl-10 sm:pl-3 text-4xl hover:text-green-500 ease-in-out duration-300" href='/'>Trickfilm400</a>
     <p class="flex-grow"></p>
-    <a class="pr-3 hover:text-gray-300 hover:underline" href="https://github.com/trickfilm400">GitHub</a>
-    <a class="pr-3 hover:text-gray-300 hover:underline" href="https://gitlab.com/trickfilm400">Gitlab</a>
-    <a class="pr-3 hover:text-gray-300 hover:underline" href="https://twitter.com/trickfilm400">Twitter</a>
+    <a class="pr-3 hover:text-green-500 ease-in-out duration-300" href="https://github.com/trickfilm400">GitHub</a>
+    <a class="pr-3 hover:text-green-500 ease-in-out duration-300" href="https://gitlab.com/trickfilm400">Gitlab</a>
+    <a class="pr-3 hover:text-green-500 ease-in-out duration-300" href="https://twitter.com/trickfilm400">Twitter</a>
   </div>
 </template>
 
