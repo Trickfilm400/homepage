62426a607858304e21c4bb0376e084aacb80360a
Nico Wagner <info@ni-wa.de>
2021-08-27 16:55:58 +0200

    added header bar with links & name with picture

---
+++ components/ProfileSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='block'>
+    <h1 class='text-8xl italic text-gray-300'>Trickfilm400</h1>
+    <div class='pt-14'>
+    <img class='w-full h-80 rounded-2xl border-black border-2' src='/pb.JPG' alt='ProfilePicture'>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileSection'
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="w-100 h-100">
-    <div class="body">
-      <Header/>
+  <div class='w-100 h-100'>
+    <Header />
+    <div class='flex justify-center flex-wrap '>
+      <ProfileSection />
     </div>
-<!--    <Tutorial/>-->
   </div>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import Vue from 'vue'
+import ProfileSection from '~/components/ProfileSection.vue'
+import Header from '~/components/Header.vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: { Header, ProfileSection }
+})
 </script>
+
+
+<style>
+body {
+  background-color: #121212;
+}
+
+</style>
