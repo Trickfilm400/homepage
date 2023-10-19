25f6ded71b62761fa6e56a8a55fc7250c60e7b4e
Nico Wagner <info@ni-wa.de>
2023-10-19 15:53:58 +0200

    feat: content update

--- components/ProfileSection.vue
+++ components/ProfileSection.vue
@@ -8,14 +8,14 @@
     <p class='text-white text-center pt-6'>
       I am a somewhat professional developer and learned coding myself <br />
       I prefer backend development, because I cannot design UI's, they always look awful :D <br /> <br />
-      Interested in DevOps and Developing Node.js Applications in Typescript, using MongoDB / Mysql / Redis and unit-tests, <br />
+      Interested in DevOps and Developing containerized Node.js Applications in Typescript, using MongoDB / Mysql / Redis and unit-tests, <br />
       also I am a huge fan of <span class='font-bold'>Docker and Kubernetes</span><br />
-      At the moment, I am trying to code some C# and .NET Core
+      At the moment, besides developing some Node.js applications, I play around with microcontrollers (ESP32) and a bit smarthome stuff
     </p>
     <br />
     <h2 class='text-white text-center text-3xl'>Contact</h2>
     <a href='mailto: info@trickfilm400.de' class='text-gray-400 underline hover:text-green-500 ease-in-out duration-300 text-center block'>info@trickfilm400.de</a>
-    <p class='text-white text-center'>Discord: Trickfilm400#7579</p>
+    <p class='text-white text-center'>Discord: @trickfilm400</p>
   </div>
 </template>
 
--- components/ProjectList.vue
+++ components/ProjectList.vue
@@ -2,12 +2,14 @@
   <div class='pt-4'>
     <h1 class='text-white text-8xl text-center'>Projects</h1>
     <div class='mt-12'>
-      <git-hub-card lang='ts' starscount='2' link-title='vantage-node' link-url='https://github.com/Trickfilm400/vantage-node' description='Get Data of WeatherStation "Vantage" & Vantage Pro (2)" by Davis instruments via Telnet'/>
+      <git-hub-card lang='ts' starscount='3' link-title='vantage-node' link-url='https://github.com/Trickfilm400/vantage-node' description='Get Data of WeatherStation "Vantage" & Vantage Pro (2)" by Davis instruments via Telnet'/>
       <git-hub-card lang='shell' starscount='0' link-title='generic-nodejs-docker' link-url='https://github.com/Trickfilm400/generic-nodejs-docker' description='A nodejs docker image to run a project without building a new docker everytime'/>
       <git-hub-card link-title='This HomePage' starscount="0" link-url='https://github.com/Trickfilm400/homepage' description='My Homepage with Nuxt.JS' lang='vue' />
-      <git-hub-card lang='ts' starscount="0" link-title='simple-dmx-control' link-url='https://github.com/Trickfilm400/simple-dmx-control' description='Simple Web-App to control lights over DMX with a Tinkerforge DMX Bricket' />
-      <git-hub-card lang='ts' starscount="0" link-title='advent-calendar-api' link-url='https://github.com/Trickfilm400/advent-calendar-api' description='A simple and small API for any advent-caneldar-website to fetch some kind of data' />
+      <git-hub-card lang='ts' starscount="0" link-title='senertec-dachs-msr2' link-url='https://github.com/Trickfilm400/senertec-dachs-msr2' description='SenerTec Dachs MSR2 GLT-Ethernet REST API Client' />
+      <git-hub-card lang='ts' starscount="0" link-title='typescript-project-scaffolding' link-url='https://github.com/Trickfilm400/typescript-project-scaffolding' description='Create a new typescript project with everything you need to your needs customized in seconds!' />
     </div>
+    <h1 class='text-white text-4xl text-center'>and a lot more...</h1>
+    <h1 class='text-white text-2xl text-center'>...see them on GitHub</h1>
   </div>
 </template>
 
